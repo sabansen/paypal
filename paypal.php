76eaddd156e9a2e87f46ec7d8302535d5d1084e9
Iuliia Fedotchenko <ifedotchenko@202-ecommerce.com>
2019-05-13 14:42:48 +0200

    ref #18101: update version

--- paypal.php
+++ paypal.php
@@ -147,7 +147,7 @@ class PayPal extends PaymentModule
     {
         $this->name = 'paypal';
         $this->tab = 'payments_gateways';
-        $this->version = '4.5.0';
+        $this->version = '@version@';
         $this->author = 'PrestaShop';
         $this->display = 'view';
         $this->module_key = '336225a5988ad434b782f2d868d7bfcd';
