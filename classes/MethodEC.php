f617166f98588f432533d62760692fed313484ff
florianthomi <51330023+florianthomi@users.noreply.github.com>
2019-06-17 13:21:07 +0200

    initialize tax index in discount
    correction for commit 308f232efce7562ce4c754c51e3d8ef645409190

--- classes/MethodEC.php
+++ classes/MethodEC.php
@@ -584,6 +584,7 @@ class MethodEC extends AbstractMethodPaypal
                     $order_total_with_reduction -= $discount['value_real'];
                 }
 
+                $discount['tax'] = -1 * $this->formatPrice($discount['value_real'] - $discount['value_tax_exc']);
                 $discount['value_real'] = -1 * $this->formatPrice($discount['value_real']);
                 $itemDetails = new PaymentDetailsItemType();
                 $itemDetails->Name = $discount['name'];
@@ -592,7 +593,7 @@ class MethodEC extends AbstractMethodPaypal
                 $itemDetails->Quantity = 1;
                 $this->_paymentDetails->PaymentDetailsItem[] = $itemDetails;
                 $this->_itemTotalValue += $discount['value_real'];
-                $this->_taxTotalValue += $this->formatPrice($discount['tax']);
+                $this->_taxTotalValue += $discount['tax'];
             }
         }
     }
