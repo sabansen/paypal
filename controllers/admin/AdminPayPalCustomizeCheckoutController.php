023a6a0b70ad94902f258ef24a767f64f90504d9
Bogdan Yampolskiy <byampolskiy@202-ecommerce.com>
2019-06-24 16:40:07 +0300

    refs #18526 page customize checkout

--- controllers/admin/AdminPayPalCustomizeCheckoutController.php
+++ controllers/admin/AdminPayPalCustomizeCheckoutController.php
@@ -22,8 +22,99 @@
  * @license   Commercial license
  * @version   develop
  */
+use PaypalAddons\classes\AdminPayPalController;
 
-class AdminPayPalCustomizeCheckoutController extends ModuleAdminController
+class AdminPayPalCustomizeCheckoutController extends AdminPayPalController
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->parametres = array(
+            'paypal_express_checkout_in_context',
+            'paypal_api_advantages',
+            'paypal_config_brand'
+        );
+    }
 
+    public function initContent()
+    {
+        $this->initForm();
+        $this->content = $this->renderForm();
+        $this->context->smarty->assign('content', $this->content);
+    }
+
+    public function initForm()
+    {
+        $this->fields_form[]['form'] = array(
+            'legend' => array(
+                'title' => $this->l('Behavior'),
+                'icon' => 'icon-cogs',
+            ),
+            'input' => array(
+                array(
+                    'type' => 'select',
+                    'label' => $this->l('PayPal In-Context'),
+                    'name' => 'paypal_express_checkout_in_context',
+                    'hint' => $this->l('PayPal opens in a pop-up window, allowing your buyers to finalize their payment without leaving your website. Optimized, modern and reassuring experience which benefits from the same security standards than during a redirection to the PayPal website.'),
+                    'options' => array(
+                        'query' => array(
+                            array(
+                                'id' => '1',
+                                'name' => $this->l('IN-CONTEXT'),
+                            ),
+                            array(
+                                'id' => '0',
+                                'name' => $this->l('REDIRECT'),
+                            )
+                        ),
+                        'id' => 'id',
+                        'name' => 'name'
+                    )
+                ),
+                array(
+                    'type' => 'html',
+                    'label' => $this->l('PayPal Express CheckoutShortcut on'),
+                    'hint' => $this->l('The PayPal shortcut is displayed directly in the cart or on your product pages, allowing a faster checkout experience for your buyers. It requires fewer pages, clicks and seconds in order to finalize the payment. PayPal provides you with the client’s billing and shipping information so that you don’t have to collect it yourself.'),
+                    'name' => '',
+                    'html_content' => ''
+                ),
+                array(
+                    'type' => 'switch',
+                    'label' => $this->l('Show PayPal benefits to your customers'),
+                    'name' => 'paypal_api_advantages',
+                    'is_bool' => true,
+                    'hint' => $this->l('You can increase your conversion rate by presenting PayPal benefits to your customers on payment methods selection page.'),
+                    'values' => array(
+                        array(
+                            'id' => 'paypal_api_advantages_on',
+                            'value' => 1,
+                            'label' => $this->l('Enabled'),
+                        ),
+                        array(
+                            'id' => 'paypal_api_advantages_off',
+                            'value' => 0,
+                            'label' => $this->l('Disabled'),
+                        )
+                    ),
+                ),
+                array(
+                    'type' => 'text',
+                    'label' => $this->l('Brand name'),
+                    'name' => 'paypal_config_brand',
+                    'placeholder' => $this->l('Leave it empty to use your Shop name'),
+                    'hint' => $this->l('A label that overrides the business name in the PayPal account on the PayPal pages.', get_class($this)),
+                ),
+            ),
+            'submit' => array(
+                'title' => $this->l('Save'),
+                'class' => 'btn btn-default pull-right button',
+            ),
+        );
+
+        $values = array();
+        foreach ($this->parametres as $parametre) {
+            $values[$parametre] = Configuration::get(Tools::strtoupper($parametre));
+        }
+        $this->tpl_form_vars = array_merge($this->tpl_form_vars, $values);
+    }
 }
