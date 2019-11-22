1d8209cac20639d3243845eeafb773b78164fae5
Bogdan Yampolskiy <byampolskiy@202-ecommerce.com>
2019-11-22 17:18:46 +0200

    refs #21270 small fix

--- classes/MethodEC.php
+++ classes/MethodEC.php
@@ -146,7 +146,6 @@ class MethodEC extends AbstractMethodPaypal
             Configuration::updateValue('PAYPAL_'.$mode.'_ACCESS', 1);
             Configuration::updateValue('PAYPAL_MERCHANT_ID_'.$mode, $params['merchant_id']);
             Configuration::updateValue('PAYPAL_EXPRESS_CHECKOUT_IN_CONTEXT', 1);
-            Configuration::updateValue('PAYPAL_API_CARD', $params['with_card']);
             $this->checkCredentials();
             return;
         }
