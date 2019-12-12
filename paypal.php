fbe24e0f8ea43b539494fd3d7cf3adb75a49b69f
Bogdan Yampolskiy <byampolskiy@202-ecommerce.com>
2019-12-12 16:10:47 +0200

    refs #21643 error while enable / disable a module

--- paypal.php
+++ paypal.php
@@ -1827,36 +1827,6 @@ class PayPal extends \PaymentModule
         return true;
     }
 
-    public function disable($force_all = false)
-    {
-        $result = true;
-        $result &= parent::disable($force_all);
-        $tabParent = \Tab::getInstanceFromClassName('AdminParentPaypalConfiguration');
-
-        if (\Validate::isLoadedObject($tabParent) == false) {
-            return $result;
-        }
-
-        $tabParent->active = false;
-        $result &= $tabParent->save();
-        return $result;
-    }
-
-    public function enable($force_all = false)
-    {
-        $result = true;
-        $result &= parent::enable($force_all);
-        $tabParent = \Tab::getInstanceFromClassName('AdminParentPaypalConfiguration');
-
-        if (\Validate::isLoadedObject($tabParent) == false) {
-            return $result;
-        }
-
-        $tabParent->active = true;
-        $result &= $tabParent->save();
-        return $result;
-    }
-
     /**
     * @return array return the unregistered hooks
      */
