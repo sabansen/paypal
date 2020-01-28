a4eb36a200bec7abeeab62821541138e037b259f
Florian <flegars@202-ecommerce.com>
2020-01-28 16:57:37 +0100

    refs #22204 - fix tax for multi currencies

--- classes/MethodEC.php
+++ classes/MethodEC.php
@@ -193,7 +193,11 @@ class MethodEC extends AbstractMethodPaypal
             $this->_paymentDetails->ShipToAddress = $address;
         }
 
-        /** The total cost of the transaction to the buyer. If shipping cost and tax charges are known, include them in this value. If not, this value should be the current subtotal of the order. If the transaction includes one or more one-time purchases, this field must be equal to the sum of the purchases. If the transaction does not include a one-time purchase such as when you set up a billing agreement for a recurring payment, set this field to 0.*/
+        /** The total cost of the transaction to the buyer. If shipping cost and tax charges are known, include them in this value.
+         * If not, this value should be the current subtotal of the order. If the transaction includes one or more one-time purchases,
+         * this field must be equal to the sum of the purchases.
+         * If the transaction does not include a one-time purchase such as when you set up a billing agreement for a recurring payment, set this field to 0.
+         */
         $this->_getPaymentDetails();
         $this->_paymentDetails->PaymentAction = Tools::ucfirst(Configuration::get('PAYPAL_API_INTENT'));
         $setECReqDetails = new SetExpressCheckoutRequestDetailsType();
@@ -225,6 +229,7 @@ class MethodEC extends AbstractMethodPaypal
 
         $setECReq = new SetExpressCheckoutReq();
         $setECReq->SetExpressCheckoutRequest = $setECReqType;
+
         /*
          * 	 ## Creating service wrapper object
         Creating service wrapper object to make API call and loading
@@ -272,7 +277,7 @@ class MethodEC extends AbstractMethodPaypal
             $itemDetails->Name = $product['name'];
             $itemDetails->Amount = $itemAmount;
             $itemDetails->Quantity = $product['quantity'];
-            $itemDetails->Tax = new BasicAmountType($currency, $this->formatPrice($product['product_tax']));
+            $itemDetails->Tax = new BasicAmountType($currency, number_format($product['product_tax'], Paypal::getDecimal(), ".", ''));
             $this->_paymentDetails->PaymentDetailsItem[] = $itemDetails;
             $this->_itemTotalValue += $this->formatPrice($product['price']) * $product['quantity'];
             $this->_taxTotalValue += $product['product_tax'] * $product['quantity'];
