87cb09f4ad60549e6722251e273e09a20befa7bc
Bogdan Yampolskiy <byampolskiy@202-ecommerce.com>
2020-05-14 10:39:50 +0200

    refs #22906 added phrase "Download PrestaShop Checkout"

--- translations/nl.php
+++ translations/nl.php
@@ -383,3 +383,4 @@ $_MODULE['<{paypal}prestashop>adminpaypalhelpcontroller_9bf2bcf8bbb1dc76aa7003ac
 $_MODULE['<{paypal}prestashop>help_553637bf7bc0c5a7193297324578e474'] = 'Deze module biedt klanten de mogelijkheid te betalen met hun PayPal-account. ';
 $_MODULE['<{paypal}prestashop>help_44beb4f75b7808f19a23cbbfd15c8bdb'] = 'Als u naast PayPal ook andere betaalmethoden wenst te accepteren, adviseren wij de PrestaShop Checkout module.';
 $_MODULE['<{paypal}prestashop>prestashopcheckoutinfo_65316efa7409b87099260ad5e3556708'] = 'Deze module biedt klanten de mogelijkheid te betalen met hun PayPal-account. Als u naast PayPal ook andere betaalmethoden wenst te accepteren, adviseren wij de  [a @href1@]PrestaShop Checkout[/a] module.';
+$_MODULE['<{paypal}prestashop>adminpaypalhelpcontroller_ca89e348869638cc70a085475a3be8c5'] = 'Download PrestShop Checkout';
