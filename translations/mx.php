415991e163186931b9b83cfbba81abe5d89260da
Bogdan Yampolskiy <byampolskiy@202-ecommerce.com>
2021-10-27 10:08:25 +0300

    refs #31767 translations

---
+++ translations/mx.php
@@ -0,0 +1,13 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{paypal}prestashop>back_office_dded4ff4c6103f5b41af5a17c215d75c'] = 'Pago en 3x';
+$_MODULE['<{paypal}prestashop>back_office_0822861afd7b07b0dc60ec6b127cb143'] = 'https://www.paypal.com/bizsignup/?country.x=MX';
+$_MODULE['<{paypal}prestashop>installmentsettings_1b139287ea9bd54f5252e7868dae1f40'] = 'Muestre el Pago en 3X de PayPal en su sitio web';
+$_MODULE['<{paypal}prestashop>installmentsettings_b0a144a9a1cbd31bbc841d2656e20e51'] = 'Muestre mensajes de pago posterior en su sitio para ofertas como Pago en 3, que permite a los clientes pagar con 3 cuotas mensuales sin intereses.';
+$_MODULE['<{paypal}prestashop>installmentsettings_0d2dd5928e3d293dfe139a73840ecb6e'] = 'Mostraremos mensajes en su sitio para promocionar esta funcin por usted. No puede promocionar las ofertas de pago posterior con ningn otro contenido, marketing o material.';
+$_MODULE['<{paypal}prestashop>installmentsettings_12cd357e7240fc5d8bd9884dec5c3a38'] = 'Habilitar la visualización de banners 3x';
+$_MODULE['<{paypal}prestashop>installmentsettings_2884ea0466e67dd75144a184a3df5054'] = 'REST Client ID Sandbox';
+$_MODULE['<{paypal}prestashop>installmentsettings_ff2be5cf3891e5b5c7e937519c6e3a22'] = 'REST Client ID';
+$_MODULE['<{paypal}prestashop>installmentsettings_b4e962d20b8b7edfe2ce508713d941c0'] = 'Mostraremos mensajes en su sitio para promocionar esta función por usted. No puede promocionar las ofertas de pago posterior con ningún otro contenido, marketing o material.';
