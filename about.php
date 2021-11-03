0ebcf73c23e2aab005789bb1396e0ebd4e7b0745
Bogdan Yampolskiy <byampolskiy@202-ecommerce.com>
2021-11-03 19:03:29 +0200

    refs #31934 license

--- about.php
+++ about.php
@@ -1,29 +1,29 @@
 <?php
 /**
- * 2007-2018 PrestaShop
  *
- * NOTICE OF LICENSE
+ *  2007-2021 PayPal
  *
- * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/afl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
+ *  NOTICE OF LICENSE
  *
- * DISCLAIMER
+ *  This source file is subject to the Academic Free License (AFL 3.0)
+ *  that is bundled with this package in the file LICENSE.txt.
+ *  It is also available through the world-wide-web at this URL:
+ *  http://opensource.org/licenses/afl-3.0.php
+ *  If you did not receive a copy of the license and are unable to
+ *  obtain it through the world-wide-web, please send an email
+ *  to license@prestashop.com so we can send you a copy immediately.
  *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
+ *  DISCLAIMER
  *
- *  @author 2007-2019 PayPal
- *  @author 2007-2013 PrestaShop SA <contact@prestashop.com>
- *  @author 2014-2019 202 ecommerce <tech@202-ecommerce.com>
+ *  Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ *  versions in the future. If you wish to customize PrestaShop for your
+ *  needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author 2007-2021 PayPal
+ *  @author 202 ecommerce <tech@202-ecommerce.com>
  *  @copyright PayPal
  *  @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- *  
+ *
  */
 
 require_once dirname(__FILE__).'/../../config/config.inc.php';
--- classes/InstallmentBanner/BannerManager.php
+++ classes/InstallmentBanner/BannerManager.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * 2007-2021 PayPal
+ *
+ *  2007-2021 PayPal
  *
  *  NOTICE OF LICENSE
  *
@@ -18,10 +19,11 @@
  *  versions in the future. If you wish to customize PrestaShop for your
  *  needs please refer to http://www.prestashop.com for more information.
  *
- *  @author 2007-2020 PayPal
+ *  @author 2007-2021 PayPal
  *  @author 202 ecommerce <tech@202-ecommerce.com>
  *  @copyright PayPal
  *  @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ *
  */
 
 require_once 'Banner.php';
