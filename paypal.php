78d4f39054c8a6a49c9b5d936bf51c04b173341e
Bogdan Yampolskiy <byampolskiy@202-ecommerce.com>
2022-03-25 10:56:03 +0200

    refs #33884 webhook. order status handling

--- paypal.php
+++ paypal.php
@@ -1766,6 +1766,7 @@ class PayPal extends \PaymentModule implements WidgetInterface
     {
         /** @var $orderPayPal PaypalOrder */
         $orderPayPal = PaypalOrder::loadByOrderId($params['id_order']);
+        $isRequestSent = false;
 
         if (!Validate::isLoadedObject($orderPayPal) || $orderPayPal->method == 'BT') {
             return false;
@@ -1810,6 +1811,7 @@ class PayPal extends \PaymentModule implements WidgetInterface
             $response = $method->void($orderPayPal);
 
             if ($response->isSuccess()) {
+                $isRequestSent = true;
                 if (Validate::isLoadedObject($paypalCapture)) {
                     $paypalCapture->result = 'voided';
                     $paypalCapture->save();
@@ -1881,6 +1883,7 @@ class PayPal extends \PaymentModule implements WidgetInterface
             $refundResponse = $method->refund($orderPayPal);
 
             if ($refundResponse->isSuccess()) {
+                $isRequestSent = true;
                 if (Validate::isLoadedObject($capture)) {
                     $capture->result = 'refunded';
                     $capture->save();
@@ -1935,6 +1938,7 @@ class PayPal extends \PaymentModule implements WidgetInterface
             $response = $method->confirmCapture($orderPayPal);
 
             if ($response->isSuccess()) {
+                $isRequestSent = true;
                 $orderPayPal->payment_status = $response->getStatus();
                 $capture->id_capture = $response->getIdTransaction();
                 $capture->result = $response->getStatus();
@@ -1974,7 +1978,7 @@ class PayPal extends \PaymentModule implements WidgetInterface
             }
         }
 
-        if ($this->getWebhookOption()->isEnable() && $this->getWebhookOption()->isAvailable()) {
+        if ($this->getWebhookOption()->isEnable() && $this->getWebhookOption()->isAvailable() && $isRequestSent) {
             try {
                 $this->getWebhookService()->createForOrder($orderPayPal, $params['newOrderStatus']->id);
             } catch (Exception $e) {
